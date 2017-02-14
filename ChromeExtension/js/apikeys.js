1b921e11f6cf60bb9dfcab7ca969f57ddf69337f
Manuel Messon-Roque <manuelmesson@gmail.com>
2017-02-14 11:08:29 -0800

    apiKeys.js changed to ko.js

--- ChromeExtension/js/apikeys.js
+++ ChromeExtension/js/apikeys.js
@@ -1,30 +1,76 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // if (localStorage["apiKey"] == null && localStorage["apiSecret"] == null) {
-    //     localStorage.setItem("apiKey", "");
-    //     localStorage.setItem("apiSecret", "");
+// document.addEventListener('DOMContentLoaded', function () {
+//     // if (localStorage["apiKey"] == null && localStorage["apiSecret"] == null) {
+//     //     localStorage.setItem("apiKey", "");
+//     //     localStorage.setItem("apiSecret", "");
 
-    // }
+//     // }
 
-    $(document).ready(function () {
-        $('#tutorial').on('click', 'a', function () {
-            chrome.tabs.create({
-                url: $(this).attr('href')
-            });
-            return false;
-        });
+//     $(document).ready(function () {
+//         $('#tutorial').on('click', 'a', function () {
+//             chrome.tabs.create({
+//                 url: $(this).attr('href')
+//             });
+//             return false;
+//         });
 
-    });
-
-
-
-    document.getElementById("apiKey").value = localStorage["apiKey"];
-    document.getElementById("apiSecret").value = localStorage["apiSecret"];
+//     });
 
 
 
-    var saveButton = document.getElementById('saveButton');
+//     // document.getElementById("apiKey").value = localStorage["apiKey"];
+//     // document.getElementById("apiSecret").value = localStorage["apiSecret"];
 
-    apiKey.addEventListener('change', function () {
+
+
+//     // var saveButton = document.getElementById('saveButton');
+
+    // apiKey.addEventListener('change', function () {
+    //     var apiKey = document.getElementById("apiKey").value;
+    //     localStorage.setItem("apiKey", apiKey);
+    // });
+
+    // apiSecret.addEventListener('change', function () {
+    //     var apiKey = document.getElementById("apiSecret").value;
+    //     localStorage.setItem("apiSecret", apiKey);
+    // });
+
+//     // onClick funcion
+//     // saveButton.addEventListener('click', function () {
+//     //     var apiKey = document.getElementById("apiKey").value;
+//     //     var apiSecret = document.getElementById("apiSecret").value;
+//     //     if (apiKey != null && apiSecret != null && apiKey != "" && apiSecret != "") {
+//     //         localStorage.setItem("apiKey", apiKey);
+//     //         localStorage.setItem("apiSecret", apiSecret);
+//     //         chrome.browserAction.setPopup({
+//     //             popup: "groups.html"
+//     //         });
+
+
+//     //     } else {
+//     //         $("#dialog").dialog({
+//     //             draggable: false,
+//     //             modal: true
+//     //         });
+
+//     //         $("#networkError").html("Oops! Those keys don't appear to be right. Please double check your API Key and Secret.");
+
+
+//     //     }
+//     //     console.log("save button works");
+
+//     // });
+
+
+
+// });
+
+function apiKeyViewModel() {
+    if (localStorage["apiKey"] == null && localStorage["apiSecret"] == null) {
+        localStorage.setItem("apiKey", "");
+        localStorage.setItem("apiSecret", "");
+
+    }
+        apiKey.addEventListener('change', function () {
         var apiKey = document.getElementById("apiKey").value;
         localStorage.setItem("apiKey", apiKey);
     });
@@ -34,37 +80,6 @@ document.addEventListener('DOMContentLoaded', function () {
         localStorage.setItem("apiSecret", apiKey);
     });
 
-    // onClick funcion
-    // saveButton.addEventListener('click', function () {
-    //     var apiKey = document.getElementById("apiKey").value;
-    //     var apiSecret = document.getElementById("apiSecret").value;
-    //     if (apiKey != null && apiSecret != null && apiKey != "" && apiSecret != "") {
-    //         localStorage.setItem("apiKey", apiKey);
-    //         localStorage.setItem("apiSecret", apiSecret);
-    //         chrome.browserAction.setPopup({
-    //             popup: "groups.html"
-    //         });
-
-
-    //     } else {
-    //         $("#dialog").dialog({
-    //             draggable: false,
-    //             modal: true
-    //         });
-
-    //         $("#networkError").html("Oops! Those keys don't appear to be right. Please double check your API Key and Secret.");
-
-
-    //     }
-    //     console.log("save button works");
-
-    // });
-
-
-
-});
-
-function apiKeyViewModel() {
     var self = this;
     self.apiKey = ko.observable(localStorage["apiKey"]);
     self.apiSecret = ko.observable(localStorage["apiSecret"]);
@@ -91,14 +106,14 @@ function apiKeyViewModel() {
     self.saveKeys = function () {
         var apiKey = document.getElementById("apiKey").value;
         var apiSecret = document.getElementById("apiSecret").value;
-        if (apiKey != null && apiSecret != null && apiKey != "" && apiSecret != "") {
+        if (self.apiKey() != null && self.apiSecret() != null && self.apiKey() != "" && self.apiSecret() != "") {
             localStorage.setItem("apiKey", apiKey);
             localStorage.setItem("apiSecret", apiSecret);
             chrome.browserAction.setPopup({
                 popup: "groups.html"
             });
 
-
+        
         } else {
             $("#dialog").dialog({
                 draggable: false,
