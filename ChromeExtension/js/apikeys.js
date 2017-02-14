bd1100d83d8c623225904b294a68d1b7a42653e7
Manuel Messon-Roque <manuelmesson@gmail.com>
2017-02-14 10:36:59 -0800

    added ko to click botton

--- ChromeExtension/js/apikeys.js
+++ ChromeExtension/js/apikeys.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    if (localStorage["apiKey"] == null && localStorage["apiSecret"] == null) {
-        localStorage.setItem("apiKey", "");
-        localStorage.setItem("apiSecret", "");
+    // if (localStorage["apiKey"] == null && localStorage["apiSecret"] == null) {
+    //     localStorage.setItem("apiKey", "");
+    //     localStorage.setItem("apiSecret", "");
 
-    }
+    // }
 
     $(document).ready(function () {
         $('#tutorial').on('click', 'a', function () {
@@ -35,7 +35,60 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     // onClick funcion
-    saveButton.addEventListener('click', function () {
+    // saveButton.addEventListener('click', function () {
+    //     var apiKey = document.getElementById("apiKey").value;
+    //     var apiSecret = document.getElementById("apiSecret").value;
+    //     if (apiKey != null && apiSecret != null && apiKey != "" && apiSecret != "") {
+    //         localStorage.setItem("apiKey", apiKey);
+    //         localStorage.setItem("apiSecret", apiSecret);
+    //         chrome.browserAction.setPopup({
+    //             popup: "groups.html"
+    //         });
+
+
+    //     } else {
+    //         $("#dialog").dialog({
+    //             draggable: false,
+    //             modal: true
+    //         });
+
+    //         $("#networkError").html("Oops! Those keys don't appear to be right. Please double check your API Key and Secret.");
+
+
+    //     }
+    //     console.log("save button works");
+
+    // });
+
+
+
+});
+
+function apiKeyViewModel() {
+    var self = this;
+    self.apiKey = ko.observable(localStorage["apiKey"]);
+    self.apiSecret = ko.observable(localStorage["apiSecret"]);
+    self.showBackLink = ko.observable(false);
+    self.showHelpLink = ko.observable(true);
+    self.loadKey = function () {
+        var ak = localStorage["apiKey"];
+        if (ak == '') {
+            self.apiKey(ak);
+        }
+
+        var as = localStorage["apiKey"];
+        if (as == '') {
+            self.apiSecret(as);
+        }
+        self.loadKey();
+    }
+
+    if (localStorageHasValue("defaultGroup")) {
+        self.showBackLink(true);
+        self.showHelpLink(false);
+    }
+
+    self.saveKeys = function () {
         var apiKey = document.getElementById("apiKey").value;
         var apiSecret = document.getElementById("apiSecret").value;
         if (apiKey != null && apiSecret != null && apiKey != "" && apiSecret != "") {
@@ -56,46 +109,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
         }
-        console.log("save button works");
-
-    });
-
-
-
-});
-
-function apiKeyViewModel() {
-    var self = this;
-    self.apiKey = ko.observable('');
-    self.apiSecret = ko.observable('');
-    self.showBackLink = ko.observable(false);
-    self.showHelpLink = ko.observable(true);
-    self.loadKey = function () {
-        var ak = localStorage["apiKey"];
-        if (ak == '') {
-            self.apiKey(ak);
-        }
-
-        var as = localStorage["apiKey"];
-        if (as == '') {
-            self.apiSecret(as);
-        }
     }
-
-    if (localStorageHasValue("defaultGroup")) {
-        self.showBackLink(true); 
-        self.showHelpLink(false);
-    } 
 
 
 
 
 }
 
+
 var apiModel = new apiKeyViewModel();
 
 if (typeof testModel === 'undefined') {
     ko.applyBindings(apiModel);
+
 } else {
     testModel = apiModel;
 }
