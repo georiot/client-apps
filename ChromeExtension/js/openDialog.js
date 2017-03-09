562d1ecb575addabf73c77a1aa1830a2ac2bd353
Manuel Messon-Roque <manuelmesson@gmail.com>
2017-03-08 17:30:43 -0800

    hack for use bootstrap in alert message

--- ChromeExtension/js/openDialog.js
+++ ChromeExtension/js/openDialog.js
@@ -2,95 +2,41 @@ chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
 
     if (msg.action == 'loading') {
 
-  if ($.fn.bootstrapFunction) {
-    // bootstrap.js already included on the site
-    alert("ya hay bootstrap")
-  } else {
-    bootbox.alert("manuellll");
-  }
+        var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
+        if (!location.ancestorOrigins.contains(extensionOrigin)) {
+            var iframe = document.createElement('iframe');
+            iframe.setAttribute("scrolling", "no");
+            // Must be declared at web_accessible_resources in manifest.json
+            iframe.src = chrome.runtime.getURL('frame.html');
 
-        
-        // wrapperDiv = document.createElement("div");
-        // wrapperDiv.setAttribute("style", "position: absolute; left: 0px; top: 0px; background-color: rgb(255, 255, 255); opacity: 0.1;");
-
-        // iframeElement = document.createElement("iframe");
-        // iframeElement.setAttribute("style", "width: 0px; height: 0px");
-
-        // wrapperDiv.appendChild(iframeElement);
-
-        // modalDialogParentDiv = document.createElement("div");
-        // modalDialogParentDiv.setAttribute("style", "position: absolute; width: 350px; border: 1px solid rgb(51, 102, 153); padding: 10px; background-color: rgb(255, 255, 255); z-index: 2001; overflow: auto; text-align: center; top: 149px; left: 497px;");
-
-        // modalDialogSiblingDiv = document.createElement("div");
-
-        // modalDialogTextDiv = document.createElement("div");
-        // modalDialogTextDiv.setAttribute("style", "text-align:center");
-
-        // modalDialogTextSpan = document.createElement("span");
-        // modalDialogText.innerHTML = "Generating shortlink...";
-
-        // breakElement = document.createElement("br");
-        // imageElement = document.createElement("img");
-        // imageElement.src = chrome.extension.getURL("spinner_progress.gif");
-
-        // modalDialogTextSpan.appendChild(modalDialogText);
-        // modalDialogTextDiv.appendChild(modalDialogTextSpan);
-        // modalDialogTextDiv.appendChild(breakElement);
-        // modalDialogTextDiv.appendChild(breakElement);
-        // modalDialogTextDiv.appendChild(imageElement);
-
-        // modalDialogSiblingDiv.appendChild(modalDialogTextDiv);
-        // modalDialogParentDiv.appendChild(modalDialogSiblingDiv);
-
-        // document.body.appendChild(wrapperDiv);
-        // document.body.appendChild(modalDialogParentDiv);
-
+            // Some styles for a fancy sidebar
+            iframe.style.cssText = 'position:fixed;top:50%;left:50%; transform: translate(-50%, -50%);display:block;' +
+                'width:300px;height:100px;z-index:1000;border:1px';
+            document.body.appendChild(iframe);
+        }
+        setTimeout(function () {
+            iframe.style.visibility = "hidden";
+        }, 3000);
     }
+
 
     if (msg.action == 'linkCreated') {
-        // wrapperDiv.remove();
-        // modalDialogParentDiv.remove();
 
-        // wrapperDiv = document.createElement("div");
-        // wrapperDiv.setAttribute("style", "position: absolute; left: 0px; top: 0px; background-color: rgb(255, 255, 255); opacity: 0.1;");
+        var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
+        if (!location.ancestorOrigins.contains(extensionOrigin)) {
+            var iframe = document.createElement('iframe');
+            iframe.setAttribute("scrolling", "no");
+            // Must be declared at web_accessible_resources in manifest.json
+            iframe.src = chrome.runtime.getURL('frame2.html');
 
-        // iframeElement = document.createElement("iframe");
-        // iframeElement.setAttribute("style", "width: 0px; height: 0px");
+            // Some styles for a fancy sidebar
+            iframe.style.cssText = 'position:fixed;top:50%;left:50%; transform: translate(-50%, -50%);display:block;' +
+                'width:300px;height:100px;z-index:1000;border:1px';
+            document.body.appendChild(iframe);
+        }
 
-        // wrapperDiv.appendChild(iframeElement);
-
-        // modalDialogParentDiv = document.createElement("div");
-        // modalDialogParentDiv.setAttribute("style", "position: absolute; width: 350px; border: 1px solid rgb(51, 102, 153); padding: 10px; background-color: rgb(255, 255, 255); z-index: 2001; overflow: auto; text-align: center; top: 149px; left: 497px;");
-
-        // modalDialogSiblingDiv = document.createElement("div");
-
-        // modalDialogTextDiv = document.createElement("div");
-        // modalDialogTextDiv.setAttribute("style", "text-align:center");
-
-        // modalDialogTextSpan = document.createElement("span");
-        // modalDialogText = document.createElement("strong");
-        // modalDialogText.innerHTML = "Se creo, meowwww!!!";
-
-        // breakElement = document.createElement("br");
-        // imageElement = document.createElement("img");
-        // imageElement.src = chrome.extension.getURL("spinner_progress.gif");
-
-        // modalDialogTextSpan.appendChild(modalDialogText);
-        // modalDialogTextDiv.appendChild(modalDialogTextSpan);
-        // modalDialogTextDiv.appendChild(breakElement);
-        // modalDialogTextDiv.appendChild(breakElement);
-        // modalDialogTextDiv.appendChild(imageElement);
-
-        // modalDialogSiblingDiv.appendChild(modalDialogTextDiv);
-        // modalDialogParentDiv.appendChild(modalDialogSiblingDiv);
-
-        // document.body.appendChild(wrapperDiv);
-        // document.body.appendChild(modalDialogParentDiv);
-
-        // setTimeout(function () {
-        //     wrapperDiv.remove();
-        //     modalDialogParentDiv.remove();
-        // }, 2000);
+        setTimeout(function () {
+            iframe.style.visibility = "hidden";
+        }, 3000);
     }
-
 });
