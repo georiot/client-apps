d392b42450782204698f16804bcc8271a15f79fb
Manuel Messon-Roque <manuelmesson@gmail.com>
2017-03-09 08:14:54 -0800

    fixed bud

--- ChromeExtension/js/openDialog.js
+++ ChromeExtension/js/openDialog.js
@@ -6,12 +6,12 @@ chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
         if (!location.ancestorOrigins.contains(extensionOrigin)) {
             var iframe = document.createElement('iframe');
             iframe.setAttribute("scrolling", "no");
-            // Must be declared at web_accessible_resources in manifest.json
+
             iframe.src = chrome.runtime.getURL('frame.html');
 
-            // Some styles for a fancy sidebar
+        
             iframe.style.cssText = 'position:fixed;top:0px;right:0px;display:block;' +
-                'width:300px;height:50px;z-index:1000;frameborder:1px';
+                'width:300px;height:50px;z-index:1000;';
             document.body.appendChild(iframe);
         }
         setTimeout(function () {
@@ -26,12 +26,12 @@ chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
         if (!location.ancestorOrigins.contains(extensionOrigin)) {
             var iframe = document.createElement('iframe');
             iframe.setAttribute("scrolling", "no");
-            // Must be declared at web_accessible_resources in manifest.json
+
             iframe.src = chrome.runtime.getURL('frame2.html');
 
-            // Some styles for a fancy sidebar
+
             iframe.style.cssText = 'position:fixed;top:0px;right:0px;display:block;' +
-                'width:300px;height:50px;z-index:1000;frameborder:1px';
+                'width:300px;height:50px;z-index:1000;';
             document.body.appendChild(iframe);
         }
 
