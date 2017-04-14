311abce37907c961863c23085a5ea640d67dfeb6
Manuel Messon-Roque <manuelmesson@gmail.com>
2017-04-14 08:58:20 -0700

    improved jsvcall

--- ServiceStackClientExample/js/jsvCall.js
+++ ServiceStackClientExample/js/jsvCall.js
@@ -7,22 +7,13 @@ var client = new GeniusLinkServiceClient('https://api.geni.us/v1', apiKey, apiSe
 client.getFromService('custom-domains/domains', {
     format: 'jsv'
 }, function (resp) {
-    var ak = localStorage["selectedDomainName"];
 
-    var result = resp['Domains'];
-    for (var i = 0; i < result.length; i++) {
-        var newItem = {
-            name: result[i]['Name'],
-            id: i
-        };
-        self.domainArray.push(newItem);
-
-        if (typeof ak !== 'undefined' && ak === result[i]['Name']) {
-            self.selectedDomain(newItem);
-
-        }
-    }
+    alert(resp);
 
 }, function (error) {
-    alert(error)
+    var parseError = JSV.parse(error);
+    var error401 = parseError.ResponseStatus.ErrorCode;
+     if (error401 == 'AuthenticationException' || error401 == 'Unauthorized') {
+         alert("oops, wrong keys");
+     }
 });
