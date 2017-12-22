2867902792c80cf6f2131252b76880414e9ef367
Adrian Cohea <adriancohea@gmail.com>
2017-12-22 14:45:01 -0800

    Add function for fetching links

--- Geniuslink_Mobile/App.js
+++ Geniuslink_Mobile/App.js
@@ -8,16 +8,32 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+
+        <Text>Try and see a change.</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: {  
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+function listLinks() {
+  return fetch('https://api.geni.us/v1/links/list', {
+    method: 'GET',
+    headers: {
+      'X-Api-Key': '0831700ddf234e2cb07cfc005464f1f5',
+      'X-Api-Secret': '3320582798ae432eaf577b9139552056',
+      Accept: 'application/json'
+    }
+  }).then(response => response.json())
+    .then(responseJson => {
+      return responseJson.Results;
+    });
+}
