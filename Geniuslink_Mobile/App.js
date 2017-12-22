a21cc1afcd6a54e5bf2137ecda87662cb9d767b4
aallfredo <aa@geni.us>
2017-12-22 17:49:26 -0500

    Unga chaka

--- Geniuslink_Mobile/App.js
+++ Geniuslink_Mobile/App.js
@@ -2,16 +2,52 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+  var PageOne = React.createClass({
+    _handlePress() {
+      this.props.navigator.push({id: 2,});
+    },
+  
+    render() {
+      return (
+        <View style={[styles.container, {backgroundColor: 'green'}]}>
+         </View>
+      )
+    },
+  });
+  
+  var PageTwo = React.createClass({
+    _handlePress() {
+      this.props.navigator.pop();
+    },
+  
+    render() {
+      return (
+        <View style={[styles.container, {backgroundColor: 'purple'}]}>
+         </View>
+      )
+    },
+  });
+  
+  var SampleApp = React.createClass({
+    _renderScene(route, navigator) {
+      if (route.id === 1) {
+        return <PageOne navigator={navigator} />
+      } else if (route.id === 2) {
+        return <PageTwo navigator={navigator} />
+      }
+    },
+  
+    render() {
+      return (
+        <Navigator
+          initialRoute={{id: 1, }}
+          renderScene={this._renderScene} />
+      );
+    }
+  });
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -21,3 +57,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
